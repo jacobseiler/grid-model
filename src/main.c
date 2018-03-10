af963e58252ab3b202c622915ea87ff65877090e
jacobseiler <jseiler@swin.edu.au>
2018-03-10 22:23:55 +1100

    Changed definition of stopSnapshot.
    It will now be defined as which snapshot do we stop AFTER we have
    finished.  This will align more on the output of SAGE.  Maybe. Idk I'll
    have to think more about it.

--- src/main.c
+++ src/main.c
@@ -205,8 +205,15 @@ int main (int argc, /*const*/ char * argv[]) {
     if(myRank==0) printf("\n++++\nreading redshift list of files and outputs... ");
     redshift_list = read_redshift_list(simParam->redshift_file, num_cycles);
     if(redshift_list != NULL)
-    { 
-      num_cycles = simParam->stop_snapshot; 
+    {
+      if (simParam->inputfiles_simulation == 1)
+      { 
+        num_cycles = simParam->stop_snapshot - simParam->SimulationLowSnap + 1;
+      }
+      else
+      {
+        num_cycles = simParam->stop_snapshot + 1;
+      } 
     }
 
     if(myRank==0) printf("done\n+++\n");
