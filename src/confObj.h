ea7b4e8a1ac44b8f34bf397d143cdecb68f3cb64
jacobseiler <jseiler@swin.edu.au>
2019-01-22 14:39:55 +1100

    Redshift file

--- src/confObj.h
+++ src/confObj.h
@@ -25,7 +25,7 @@ struct confObj_struct {
     //General
     int            num_snapshots;
     int32_t        stop_snapshot;
-    char           redshift_file;
+    char           *redshift_file;
     double         redshift_prev_snap;
     double         redshift;
     double         evol_time;
